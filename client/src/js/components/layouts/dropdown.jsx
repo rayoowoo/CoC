d9b0d11d9b05fafefb1d6e13d25ce40c2ca1789b
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-07 11:46:38 -0400

    Add basic dropdowns for navbar links

--- client/src/js/components/layouts/dropdown.jsx
+++ client/src/js/components/layouts/dropdown.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 
 export default (props) => {
-    const menu = props.label === "MENU" ? "navbar-right-menu" : "";
-    console.log(props.links);
-
-    const {links, label} = props;
-
-    
+    const menu = props.label === "MENU" ? "navbar-right-menu navbar-right-link" : "navbar-right-link ";
+    const {links, label} = props
+   
     const listLinks = links ? 
-    <ul className="dropdown">
+    <ul className="dropdown" id={`${label}-dropdown`}>
     {links.map(el => {
         return <li key={`link-${el}`}>{el}</li>
     })}</ul> : null;
 
+
+    const trigger = e => {
+        e.preventDefault();
+        const dropdown = document.getElementById(`${label}-dropdown`);
+        if (dropdown) dropdown.classList.add("display");
+    }
+
+    const removeTrigger = e => {
+        e.preventDefault();
+        const dropdown = document.getElementById(`${label}-dropdown`);
+        if (dropdown) dropdown.classList.remove("display");
+    }
+
     return (
         <>
-            <li className={menu}>{label}</li>
-            {listLinks}
+            <li className={menu} onMouseOver={trigger} onMouseLeave={removeTrigger}>{label}
+                {listLinks}
+            </li>         
         </>
     )
 }
