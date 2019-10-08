3685d66abeb1647bcddfd75174316212e56c3a79
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-07 21:00:12 -0400

    Add content to about page

--- client/src/js/App.jsx
+++ client/src/js/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import NavBar from './components/layouts/navbar';
 import Footer from './components/layouts/footer';
 import Home from './components/landing/home';
+import About from './components/about/about';
 import {useRoutes} from 'hookrouter';
 
 const routes = {
-    "/": () => <Home />
+    "/": () => <Home />,
+    "/about": () => <About />
 }
 
 export default () => {
