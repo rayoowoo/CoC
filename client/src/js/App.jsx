0b473769b3f6f55c36b8e1303e721c0707f28365
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-08 11:07:33 -0400

    Add not found page

--- client/src/js/App.jsx
+++ client/src/js/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import NotFound from './components/layouts/not_found';
 import NavBar from './components/layouts/navbar';
 import Footer from './components/layouts/footer';
 import Home from './components/landing/home';
 import About from './components/about/about';
+import Stories from './components/about/stories';
 import {useRoutes} from 'hookrouter';
 
 const routes = {
     "/": () => <Home />,
-    "/about": () => <About />
+    "/about": () => <About />,
+    "/about/stories": () => <Stories />
 }
 
 export default () => {
-    const route = useRoutes(routes);
+    const route = useRoutes(routes) || <NotFound />;
     return (
         <div>
             <NavBar />
