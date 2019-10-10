492acc42dbd109a3fb17b0c21e3fe675ece13400
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-10 11:45:53 -0400

    Limit number of stories accessible to user

--- client/src/js/components/about/story_page.jsx
+++ client/src/js/components/about/story_page.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import NotFound from '../layouts/not_found';
 
 export default (props) => {
-    const {name, otherProps: {validNames}} = props;
-    // if (validNames.has(name)) {
-    //     return (
-    //         <h1>HI</h1>
-    //     )
-    // } else {
-    //     return <NotFound />
-    // }
+    const {name: {name}, otherProps: {validNames}} = props;
+    if (!validNames.has(name)) return <NotFound />;
+    
     return (
         <h1>HI</h1>
     )
+
 }
