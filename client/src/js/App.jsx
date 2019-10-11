3ccdd8fa2835d4e31135014f810e405b2697c7bf
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-11 16:46:39 -0400

    Set up activities front end routing

--- client/src/js/App.jsx
+++ client/src/js/App.jsx
@@ -5,12 +5,14 @@ import Footer from './components/layouts/footer';
 import Home from './components/landing/home';
 import About from './components/about/about';
 import Stories from './components/about/stories';
+import Activities from './components/activities/activities';
 import {useRoutes} from 'hookrouter';
 
 const routes = {
     "/": () => <Home />,
     "/about-us": () => <About />,
-    "/about-us/our-stories*": () => <Stories />
+    "/about-us/our-stories*": () => <Stories />,
+    "/activities": () => <Activities />
 }
 
 export default () => {
