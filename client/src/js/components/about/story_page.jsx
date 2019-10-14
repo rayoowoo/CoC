34fb0a86238c68224c1ec50928a97da278dcba7b
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-14 11:57:01 -0400

    Refactor tiles CSS again

--- client/src/js/components/about/story_page.jsx
+++ client/src/js/components/about/story_page.jsx
@@ -8,7 +8,7 @@ export default (props) => {
     if (name in stories) content = stories[name].content.split("/n").map(el => <p className="paragraph">{el}</p>);
 
     return (
-        <section className="stories-page">
+        <section className="stories-page-content detail-content">
             <h2 className="head2">{name}</h2>
             {content}
             <button className="button1" onClick={historyPush('/about-us/our-stories')}>Back to stories</button>
--- client/src/js/components/activities/twos_threes.jsx
+++ client/src/js/components/activities/twos_threes.jsx
@@ -2,6 +2,72 @@ import React from 'react';
 
 export default (props) => {
     return (
-        <h1>HI</h1>
+        <section className="detail-page">
+            <div className="detail-splash twos-threes-splash">
+                <h2 className="head2">Two's and Three's</h2>
+            </div>
+
+         
+
+            <section className="detail-content twos-threes-content">
+
+                <h3 className="head3 twos-threes-verse">"For where there are two or threes
+                gathered into My name, there am I in their midst"</h3>
+                <h3 className="head3 twos-threes-verse-ref">Matthew 18:20</h3>
+
+                <h3 className="head3 twos-threes-verse">"Two are better than one, because they have
+                a good reward for their labor; for if they fall, the one will lift up his companion,
+                and a threefold cord is not quickly broken."</h3>
+                <h3 className="head3 twos-threes-verse-ref">Ecclesiastes 4:9-10a, 12b</h3>
+
+                <div className="tile-set twos-threes-quotes">
+                    <div className="tile2 twos-threes-quote">
+                        <h3 className="head3">
+                            <i className="fa fa-quote-left"></i>
+                            During my freshman year, I got to know one of my spiritual companions 
+                            by meeting up with him a couple of times a week. 
+                            It set the foundation for my Christian pursuit during college.
+                            <i className="fa fa-quote-right"></i>
+                        </h3>
+                    </div>
+                    <div className="tile2 twos-threes-quote">
+                        <h3 className="head3">
+                            <i className="fa fa-quote-left"></i>
+                            To this day, I still pray with a student I met in the club here. 
+                            We can request prayer from one another at any time, in any place. 
+                            It's nice to know that I always have someone to turn to, even if 
+                            we're on opposite coasts.
+                            <i className="fa fa-quote-right"></i>
+                        </h3>
+                    </div>
+                    <div className="tile2 twos-threes-quote">
+                        <h3 className="head3">
+                            <i className="fa fa-quote-left"></i>
+                            The appointments I had with a staff member rescued my Christian 
+                            life. Without a doubt, I would not be here without those times.
+                            <i className="fa fa-quote-right"></i>
+                        </h3>
+                    </div>
+                </div>
+
+                
+
+                <p className="paragraph">At Christians on Campus, we endeavor to practice getting together 
+                in units of two and three, especially as we get busier during the semester. It's a great 
+                way to stay connected, to develop a support system, and, ultimately, to sustain our 
+                Christian pursuit while we are here in school.</p>
+
+                <p className="paragraph">Two's and Three's are great if:</p>
+                <ul className="paragraph">
+                    <li className="bullet">you're too busy to make it to any larger Christian gatherings</li>
+                    <li className="bullet">you want something with more flexibility</li>
+                    <li className="bullet">you want spiritual companions</li>
+                    <li className="bullet">you want some personal mentoring or guidance</li>
+                </ul>
+
+                <p className="paragraph">If any of these sound like you, then this is for you!</p>
+                <p className="paragraph">Contact us below for more information.</p>
+            </section>
+        </section>
     )
 }
