107031d26a55fa7a666be92044484ac89256c49c
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-15 10:15:01 -0400

    Add basic content to spring break trip page

--- client/src/js/components/activities/spring_break_trip.jsx
+++ client/src/js/components/activities/spring_break_trip.jsx
@@ -2,6 +2,31 @@ import React from 'react';
 
 export default (props) => {
     return (
-        <h1>HI</h1>
+        <section className="detail-page">
+            <div className="detail-splash sbt-splash">
+                <h2 className="head2">Spring Break Trip</h2>
+            </div>
+
+            <section className="detail-content sbt-content">
+                <div className="sbt-banner"></div>
+                {/* <button className="button2">Sign me up!</button> */}
+
+                <p className="paragraph">Every year, during spring break, Christians on Campus 
+                takes a weeklong trip to visit sisters clubs elsewhere in the nation. This week 
+                is full of travelling, sight-seeing, fun activities, and most importantly, a lot 
+                of rich fellowship with the student believers that are receiving us. It's an 
+                opportunity to see how students in other places are pursuing Christ, and to 
+                experience the reality of the Body of Christ. </p>
+
+                <h3 className="head3">Destinations</h3>
+                <h1 className="head1">Sacramento, CA</h1>
+                <h1 className="head1">Reno, NV</h1>
+                <h1 className="head1">Boise, ID</h1>
+
+                <p className="paragraph">You can find out more about what we did on our blog! It was definitely a blast!</p>
+
+                <button className="button1">See More</button>
+            </section>
+        </section>
     )
 }
