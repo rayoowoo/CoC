2757eda064b65741997406258413cb4fe1e6a804
Ruiyu <ruiyuwu1998@gmail.com>
2019-11-12 16:55:17 -0500

    Finish styling upcoming page

--- client/src/js/components/upcoming/upcoming.jsx
+++ client/src/js/components/upcoming/upcoming.jsx
@@ -8,14 +8,18 @@ export default () => {
             </div>
 
             <section className="main-content upcoming-content">
-                <div>
-                    <img src="" alt="fellowship-night-banner"/>
+                <div className="banner">
+                    <div className="banner-img">
+                        <img src="" alt="fellowship-night-banner"/>
+                    </div>
                     <h2 className="head2">Fellowship Night</h2>
                     <p className="paragraph">November 13, 2019, 7-8:30pm, 2430 Dana St.</p>
                     <p className="paragraph">Come enjoy dinner, fellowship with other believers, and a helpful speaking from what we have been studying in the Bible studies! </p>
                 </div>
-                <div>
-                    <img src="" alt="bible-studies-banner" />
+                <div className="banner">
+                    <div className="banner-img">
+                        <img src="" alt="bible-studies-banner" />
+                    </div>
                     <h2 className="head2">Bible Studies</h2>
                     <p className="paragraph">Get plugged into a Bible study group and dive into the word of God. This school year we will be studying Matthew. We start August 28!</p>
                     <p className="paragraph">Campanile Esplanade | Tuesday 2-3p<br />Contact Kevin at (###) ###-#### </p>
