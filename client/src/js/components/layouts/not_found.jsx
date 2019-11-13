2e2befae97bc0a873a55b74f51e3724fece77871
Ruiyu <ruiyuwu1998@gmail.com>
2019-11-12 22:49:29 -0500

    Finish notfound page

--- client/src/js/components/layouts/not_found.jsx
+++ client/src/js/components/layouts/not_found.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
+import {historyPush} from '../reuse/utils';
 
 export default () => (
-    <>
-        <h1>NOT FOUND</h1>
-        <h1>NOT FOUND</h1>
-        <h1>NOT FOUND</h1>
-        <h1>NOT FOUND</h1>
-        <h1>NOT FOUND</h1>
-        <h1>NOT FOUND</h1>
-    </>
+    <div className="main-page notfound">
+        <h1 className="head1">We're sorry.</h1>
+        <h3 className="head3">Either this is not a page, or it has not been implemented yet.</h3>
+        <button onClick={historyPush("/")} className="button1">Home</button>
+    </div>
 )
